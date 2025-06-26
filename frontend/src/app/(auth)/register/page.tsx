60fa33285f6c03e6b46c7d04d55ffdf7fb650223
Asgard09 <thanhphuc04nh@gmail.com>
2025-06-26 22:02:06 +0700

    feat(main page): create some main pages

---
+++ frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () =>{
+    return(
+        <div>
+            Here is register page
+        </div>
+    )
+}
+
+export default RegisterPage;
---
+++ frontend/src/app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      Here is Home Page
+    </div>
+  );
+}
