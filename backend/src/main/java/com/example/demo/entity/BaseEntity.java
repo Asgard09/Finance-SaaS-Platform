c38cfa6657475c09ef07d98fb18b643c2817e97c
Asgard09 <thanhphuc04nh@gmail.com>
2025-07-31 09:42:34 +0700

    feat: add BaseEntity (Abstract class)

---
+++ backend/src/main/java/com/example/demo/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+/*Note
+* isn't recognized as an entity and cannot be directly queried by JPA.
+* Instead, it serves as a base class to provide common fields*/
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
