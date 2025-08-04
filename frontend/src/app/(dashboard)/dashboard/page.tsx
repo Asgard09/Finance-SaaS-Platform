0670490f1438ef0803b868825f7de9af8201a71d
Asgard09 <thanhphuc04nh@gmail.com>
2025-08-04 20:34:00 +0700

    Feat: add comparison componet into dashboard page

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import { DataCharts } from "@/components/data-charts";
 import { DataGrid } from "@/components/data-grid";
+import { MonthComparison } from "@/components/month-comparison";
 
 export default function DashboardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <DataGrid />
       <DataCharts />
+      <MonthComparison />
     </div>
   );
 }
